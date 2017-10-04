faa4116b35ec07e1421b39f34fbf4f55df8489dc
Your Name <you@example.com>
2017-10-04 09:49:21 +0200

    ahora indica tambien el lugar de nacimiento

--- ejemplo.java
+++ ejemplo.java
@@ -2,6 +2,7 @@ public class ejemplo {
     public static void main (String args []){
         System.out.println("Dídac");
         System.out.println("19");
+        System.out.println("León");
     }
 
 }
